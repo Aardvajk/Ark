89bcc38789733f63a85a743f88e2aa536b0fcc4b
Paul <aardvajk@live.co.uk>
2019-03-16 08:03:12 +0000

    Fix null value bugs in TextureData property and add offset

--- core/TextureData.h
+++ core/TextureData.h
@@ -9,19 +9,19 @@
 class TextureData
 {
 public:
-    TextureData() : scale(1, 1) { }
-    explicit TextureData(const Gx::Vec2 &scale) : scale(scale) { }
+    TextureData() : scale(1, 1), offset(0, 0) { }
 
-    bool operator==(const TextureData &o) const { return scale == o.scale; }
-    bool operator!=(const TextureData &o) const { return scale != o.scale; }
+    bool operator==(const TextureData &o) const { return scale == o.scale && offset == o.offset; }
+    bool operator!=(const TextureData &o) const { return scale != o.scale || offset != o.offset; }
     bool operator<(const TextureData &o) const { return true; }
 
     Gx::Vec2 scale;
+    Gx::Vec2 offset;
 };
 
 Q_DECLARE_METATYPE(TextureData)
 
-inline QDataStream &operator<<(QDataStream &ds, const TextureData &v){ return ds << v.scale; }
-inline QDataStream &operator>>(QDataStream &ds, TextureData &v){ return ds >> v.scale; }
+inline QDataStream &operator<<(QDataStream &ds, const TextureData &v){ return ds << v.scale << v.offset; }
+inline QDataStream &operator>>(QDataStream &ds, TextureData &v){ return ds >> v.scale >> v.offset; }
 
 #endif // TEXTUREDATA_H
--- properties/types/TextureDataPropertyBrowserType.cpp
+++ properties/types/TextureDataPropertyBrowserType.cpp
@@ -4,6 +4,23 @@
 
 #include <QPxPropertyBrowser/QPxPropertyBrowserItem.h>
 
+namespace
+{
+
+QVector<QVariant> values(const QVariant &value)
+{
+    if(value.isValid())
+    {
+        auto d = value.value<TextureData>();
+
+        return { QVariant::fromValue(d.scale), QVariant::fromValue(d.offset) };
+    }
+
+    return { QVariant(), QVariant() };
+}
+
+}
+
 TextureDataPropertyBrowserType::TextureDataPropertyBrowserType(QObject *parent) : QPx::PropertyBrowserType(parent)
 {
     type = new Vec2PropertyBrowserType(this);
@@ -11,9 +28,10 @@ TextureDataPropertyBrowserType::TextureDataPropertyBrowserType(QObject *parent) 
 
 void TextureDataPropertyBrowserType::addProperties(QPx::PropertyBrowserItem *item, QPx::PropertyBrowserModel *model, const QModelIndex &parent) const
 {
-    auto v = item->value().value<TextureData>();
+    auto v = values(item->value());
 
-    item->addItem(new QPx::PropertyBrowserItem(type, model, parent, { }, "Scale", item->flags(), QVariant::fromValue(v.scale), item));
+    item->addItem(new QPx::PropertyBrowserItem(type, model, parent, { }, "Scale", item->flags(), v[0], item));
+    item->addItem(new QPx::PropertyBrowserItem(type, model, parent, { }, "Offset", item->flags(), v[1], item));
 
     for(auto i: item->items())
     {
@@ -23,9 +41,10 @@ void TextureDataPropertyBrowserType::addProperties(QPx::PropertyBrowserItem *ite
 
 void TextureDataPropertyBrowserType::updateProperties(QPx::PropertyBrowserItem *item, const QVariant &value) const
 {
-    auto v = value.value<TextureData>();
+    auto v = values(value);
 
-    item->items()[0]->setValue(QVariant::fromValue(v.scale));
+    item->items()[0]->setValue(v[0]);
+    item->items()[1]->setValue(v[1]);
 }
 
 QString TextureDataPropertyBrowserType::valueText(const QPx::PropertyBrowserItem *item) const
@@ -54,7 +73,8 @@ void TextureDataPropertyBrowserType::changed(const QVariant &value)
 
         switch(parent->items().indexOf(item))
         {
-            case 0: v.scale = value.value<Gx::Vec2>();
+            case 0: v.scale = value.value<Gx::Vec2>(); break;
+            case 1: v.offset = value.value<Gx::Vec2>(); break;
         }
 
         auto var = QVariant::fromValue(v);
